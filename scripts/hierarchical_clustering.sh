ba432941e8dba246894d2c9e2dc54ed96b1d5db5
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2024-10-28 10:48:10 +0100

    added script for hierarchical clustering

---
+++ scripts/hierarchical_clustering.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATA_FOLDER=../data
+
+bash dealign.sh $DATA_FOLDER/new_aln.faa > $DATA_FOLDER/new_dealn.faa
+
+cd-hit -i $DATA_FOLDER/new_dealn.faa -c 0.9 -o $DATA_FOLDER/ch_run1.faa
+cd-hit -i $DATA_FOLDER/ch_run1.faa -c 0.7 -o $DATA_FOLDER/ch_run2.faa
+cd-hit -i $DATA_FOLDER/ch_run2.faa -c 0.5 -n 3 -o $DATA_FOLDER/ch_run3.faa
+#cd-hit -i $DATA_FOLDER/ch_run3.faa -c 0.2 -n 2 -o $DATA_FOLDER/ch_run4.faa
